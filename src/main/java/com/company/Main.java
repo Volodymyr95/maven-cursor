8e2639107eb2a363de381f96fadd95461a164fa6
volodymyr_krokhmaliuk <krokhmalyuk777@gmail.com>
2022-07-26 20:46:13 +0300

    init comit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = sum(2, 2);
+        System.out.println(4);
+    }
+
+    public static int sum(int a, int b) {
+        return a * b;
+    }
+
+}
